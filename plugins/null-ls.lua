e561412bddc76e8adc9bd8c2f507947304ea591a
FluxxField <keenanjj13@protonmail.com>
2023-04-03 16:11:26 -0600

    null-ls update

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,16 +2,27 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostic.eslint_d,
+      null_ls.builtins.diagnostic.jshint,
+      null_ls.builtins.diagnostics.stylelint,
+      null_ls.builtins.diagnostics.tsc,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.codespell,
+      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.formatting.fixjson,
+      null_ls.builtins.formatting.gofmt,
     }
+
     return config -- return final config table
   end,
 }
