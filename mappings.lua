cbbbd75b380dbd3d9d34126fea6382c41107175f
Keenan <keenanjj13@gmail.com>
2023-05-08 15:37:47 -0500

    refactor: hop mappings

--- mappings.lua
+++ mappings.lua
@@ -4,11 +4,6 @@ local hint = require "hop.hint"
 local directions = hint.HintDirection
 local position = hint.HintPosition
 
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
 return {
   n = {
     ["<leader>b"] = { name = "Buffers" },
@@ -43,13 +38,78 @@ return {
     -- Zen Mode
     ["<leader>z"] = { "<cmd>ZenMode<cr>", desc = "Zen Mode" },
     -- Hop
-    ["<leader><leader>"] = { name = "Hop" },
-    ["<leader><leader>e"] = {
+    ["<leader><leader>o"] = {
+      function()
+        vim.cmd [[:HopLineStart]]
+        vim.schedule(function()
+          vim.cmd [[normal! o]]
+          vim.cmd [[normal! o]]
+          vim.cmd [[startinsert]]
+        end)
+      end,
+      desc = "Hop Line Insert new line below",
+    },
+    ["<leader><leader>O"] = {
+      function()
+        vim.cmd [[:HopLineStart]]
+        vim.schedule(function()
+          vim.cmd [[normal! O]]
+          vim.cmd [[normal! O]]
+          vim.cmd [[startinsert]]
+        end)
+      end,
+      desc = "Hop Line Insert new line above",
+    },
+    -- easymotion-like
+    ["w"] = {
+      function()
+        hop.hint_words {
+          direction = directions.AFTER_CURSOR,
+        }
+      end,
+      desc = "Hop Word after cursor"
+    },
+    ["b"] = {
+      function()
+        hop.hint_words {
+          direction = directions.BEFORE_CURSOR,
+        }
+      end,
+      desc = "Hop Word before cursor",
+    },
+    ["jj"] = {
+      function()
+        hop.hint_lines_skip_whitespace {
+          direction = directions.AFTER_CURSOR,
+        }
+      end,
+      desc = "Hop Line after cursor",
+    },
+    ["kk"] = {
+      function()
+        hop.hint_lines_skip_whitespace {
+          direction = directions.BEFORE_CURSOR,
+        }
+      end,
+      desc = "Hop line before cursor",
+    },
+    -- sneak-like
+    ["s"] = { "<cmd>HopChar2AC<cr>", desc = "Hop Char2 after cursor" },
+    ["S"] = { "<cmd>HopChar2BC<cr>", desc = "Hop Char2 before cursor" },
+    ["e"] = {
       function()
         hop.hint_words {
           direction = directions.AFTER_CURSOR,
           hint_position = position.END,
-          current_line_only = true,
+        }
+      end,
+      desc = "Hop end of word after cursor",
+    },
+    ["ge"] = {
+      function()
+        hop.hint_words {
+          direction = directions.BEFORE_CURSOR,
+          hint_position = position.END,
         }
       end,
       desc = "Hop end of word after cursor",
@@ -92,36 +152,6 @@ return {
       end,
       desc = "Hop to char before cursor current line offset",
     },
-    ["<leader><leader>o"] = {
-      function()
-        vim.cmd [[:HopLineStart]]
-        vim.schedule(function()
-          vim.cmd [[normal! o]]
-          vim.cmd [[normal! o]]
-          vim.cmd [[startinsert]]
-        end)
-      end,
-      desc = "Hop Line Insert new line below",
-    },
-    ["<leader><leader>O"] = {
-      function()
-        vim.cmd [[:HopLineStart]]
-        vim.schedule(function()
-          vim.cmd [[normal! O]]
-          vim.cmd [[normal! O]]
-          vim.cmd [[startinsert]]
-        end)
-      end,
-      desc = "Hop Line Insert new line above",
-    },
-    -- easymotion-like
-    ["<leader><leader>w"] = { "<cmd>HopWordAC<cr>", desc = "Hop Word after cursor" },
-    ["<leader><leader>b"] = { "<cmd>HopWordBC<cr>", desc = "Hop Word before cursor" },
-    ["<leader><leader>j"] = { "<cmd>HopLineStartAC<cr>", desc = "Hop Line after cursor" },
-    ["<leader><leader>k"] = { "<cmd>HopLineStartBC<cr>", desc = "Hop Line before cursor" },
-    -- sneak-like
-    ["s"] = { "<cmd>HopChar2AC<cr>", desc = "Hop Char2 after cursor" },
-    ["S"] = { "<cmd>HopChar2BC<cr>", desc = "Hop Char2 before cursor" },
     ["<esc>"] = false,
   },
   t = {
@@ -133,17 +163,60 @@ return {
     ["<"] = { "<gv", desc = "Shift left" },
     [">"] = { ">gv", desc = "Shift right" },
     -- Hop
-    ["<leader><leader>"] = { "Hop" },
-    ["<leader><leader>hw"] = { "<cmd>HopWord<cr>", desc = "Hop word" },
-    ["<leader><leader>hl"] = { "<cmd>HopLine<cr>", desc = "Hop line" },
     -- easymotion-like
-    ["<leader><leader>w"] = { "<cmd>HopWordAC<cr>", desc = "Hop Word after cursor" },
-    ["<leader><leader>b"] = { "<cmd>HopWordBC<cr>", desc = "Hop Word before cursor" },
-    ["<leader><leader>j"] = { "<cmd>HopLineStartAC<cr>", desc = "Hop Line after cursor" },
-    ["<leader><leader>k"] = { "<cmd>HopLineStartBC<cr>", desc = "Hop Line before cursor" },
+    ["w"] = {
+      function()
+        hop.hint_words {
+          direction = directions.AFTER_CURSOR,
+        }
+      end,
+      desc = "Hop Word after cursor"
+    },
+    ["b"] = {
+      function()
+        hop.hint_words {
+          direction = directions.BEFORE_CURSOR,
+        }
+      end,
+      desc = "Hop Word before cursor",
+    },
+    ["jj"] = {
+      function()
+        hop.hint_lines_skip_whitespace {
+          direction = directions.AFTER_CURSOR,
+        }
+      end,
+      desc = "Hop Line after cursor",
+    },
+    ["kk"] = {
+      function()
+        hop.hint_lines_skip_whitespace {
+          direction = directions.BEFORE_CURSOR,
+        }
+      end,
+      desc = "Hop line before cursor",
+    },
     -- sneak-like
     ["s"] = { "<cmd>HopChar2AC<cr>", desc = "Hop Char2 after cursor" },
     ["S"] = { "<cmd>HopChar2BC<cr>", desc = "Hop Char2 before cursor" },
+    ["e"] = {
+      function()
+        hop.hint_words {
+          direction = directions.AFTER_CURSOR,
+          hint_position = position.END,
+        }
+      end,
+      desc = "Hop end of word after cursor",
+    },
+    ["ge"] = {
+      function()
+        hop.hint_words {
+          direction = directions.BEFORE_CURSOR,
+          hint_position = position.END,
+        }
+      end,
+      desc = "Hop end of word after cursor",
+    },
     ["f"] = {
       function()
         hop.hint_char1 {
@@ -151,6 +224,7 @@ return {
           current_line_only = true,
         }
       end,
+      desc = "Hop to char after cursor current line",
     },
     ["F"] = {
       function()
@@ -158,29 +232,28 @@ return {
           direction = directions.BEFORE_CURSOR,
           current_line_only = true,
         }
-      end
-    },
-    ["<leader><leader>o"] = {
-      function()
-        vim.cmd [[:HopLineStart]]
-        vim.schedule(function()
-          vim.cmd [[normal! o]]
-          vim.cmd [[normal! o]]
-          vim.cmd [[startinsert]]
-        end)
       end,
-      desc = "Hop Line Insert new line below",
+      desc = "Hop to char before cursor current line",
     },
-    ["<leader><leader>O"] = {
+    ["t"] = {
       function()
-        vim.cmd [[:HopLineStart]]
-        vim.schedule(function()
-          vim.cmd [[normal! O]]
-          vim.cmd [[normal! O]]
-          vim.cmd [[startinsert]]
-        end)
+        hop.hint_char1({
+          direction = directions.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        })
       end,
-      desc = "Hop Line Insert new line above",
+      desc = "Hop to char after cursor current line offset",
+    },
+    ["T"] = {
+      function()
+        hop.hint_char1({
+          direction = directions.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = 1,
+        })
+      end,
+      desc = "Hop to char before cursor current line offset",
     },
     ["<esc>"] = false,
   },
