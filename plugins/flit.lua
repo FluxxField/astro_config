d3f396e2e68bf3b9b03623cde9b2682f3fcb70d8
FluxxField <keenanjj13@protonmail.com>
2024-04-29 08:11:29 -0600

    fix(flit): fixed key prop

--- plugins/flit.lua
+++ plugins/flit.lua
@@ -2,18 +2,9 @@ return {
   "ggandor/flit.nvim",
   dependiences = { "ggandor/leap.nvim", "tpope/vim-repeat" },
   cond = function() return require("user/customize").flit end,
-  keys = function()
-    local ret = {}
-
-    for _, key in ipairs { "f", "F", "t", "T" } do
-      ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = "Flit " .. key }
-    end
-
-    return ret
-  end,
   opts = {
-    label_modes = "nv",
-    keys = { "f", "F", "t", "T" },
+    label_modes = "v",
+    keys = { f = "f", F = "F", t = "t", T = "T" },
     multiline = true,
   },
   config = function(_, opts) require("flit").setup(opts) end,
