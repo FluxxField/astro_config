a274e27f2998846b44bb1e0e3d7864cf124a5ea7
FluxxField <keenanjj13@protonmail.com>
2025-03-29 13:53:12 -0600

    feat: added smart-motion plugin

---
+++ lua/plugins/smart-motion.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "FluxxField/smart-motion.nvim",
+  dir = "~/github/smart-motion.nvim",
+  ---@type SmartMotion.Config
+  opts = {
+    presets = {
+      words = true,
+      lines = true,
+      search = true,
+    },
+  },
+}
