ed3a37af6a910364a4839a823a017c01374cbe64
FluxxField <keenanjj13@protonmail.com>
2025-04-08 21:58:27 -0600

    feat(mappings): added new centered movements

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -9,7 +9,23 @@ return {
           ["<A-k>"] = { ":move '<-2<CR>gv-gv", desc = "Move block up (sticky)" },
         },
         n = {
-          -- your normal mode mappings...
+          ["<leader>zz"] = { "zz", desc = "Center current line" },
+
+          -- Search results centered
+          ["n"] = { "nzzzv", desc = "Next search result centered" },
+          ["N"] = { "Nzzzv", desc = "Prev search result centered" },
+
+          -- Scroll centered
+          ["<C-d>"] = { "<C-d>zz", desc = "Scroll down centered" },
+          ["<C-u>"] = { "<C-u>zz", desc = "Scroll up centered" },
+
+          -- Paragraph movement centered
+          ["{"] = { "{zz", desc = "Back a paragraph centered" },
+          ["}"] = { "}zz", desc = "Forward a paragraph centered" },
+
+          -- Keep cursor in center after page jump
+          ["G"] = { "Gzz", desc = "End of file centered" },
+          ["gg"] = { "ggzz", desc = "Top of file centered" },
         },
       },
     },
