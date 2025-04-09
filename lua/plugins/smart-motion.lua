600b951dc3551125f990713b10181e060a352da1
FluxxField <keenanjj13@protonmail.com>
2025-04-08 21:59:09 -0600

    refactor(smart-motion): removed presets

--- lua/plugins/smart-motion.lua
+++ lua/plugins/smart-motion.lua
@@ -3,11 +3,5 @@ return {
   "FluxxField/smart-motion.nvim",
   dir = "~/github/smart-motion.nvim",
   ---@type SmartMotion.Config
-  opts = {
-    presets = {
-      -- words = true,
-      -- lines = true,
-      -- search = true,
-    },
-  },
+  opts = {},
 }
