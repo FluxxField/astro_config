45880566976679d8e0b58f1e193dc1c0baacdb4c
Keenan <keenanjj13@gmail.com>
2025-04-08 18:43:21 -0600

    feat(treesitter): more text object support and new actions with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   opts = {
     auto_install = true,
     ensure_installed = {
@@ -11,7 +16,6 @@ return {
       "bash",
       "markdown",
       "markdown_inline",
-
       -- Web Development
       "html",
       "css",
@@ -26,7 +30,6 @@ return {
       "toml",
       "graphql",
       "http",
-
       -- Config/dotfile formats
       "ini",
       "dockerfile",
@@ -34,21 +37,61 @@ return {
       "gitignore",
       "gitattributes",
       "gitcommit",
-
       -- Programming Languages
       "rust",
       "go",
       "gomod",
       "gowork",
       "gosum",
-
       -- SQL & Databases
       "sql",
-
       -- Other formats
       "kdl",
       "regex",
       "query",
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["ao"] = "@object.outer",
+          ["io"] = "@object.inner",
+          ["ak"] = "@key.outer",
+          ["ik"] = "@key.inner",
+          ["av"] = "@value.outer",
+          ["iv"] = "@value.inner",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["at"] = "@jsx.outer",
+          ["it"] = "@jsx.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]a"] = "@parameter.outer",
+          ["]t"] = "@jsx.outer",
+        },
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[a"] = "@parameter.outer",
+          ["[t"] = "@jsx.outer",
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          [">a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<a"] = "@parameter.inner",
+        },
+      },
     },
   },
 }
