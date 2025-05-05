4a19e3c44e054cfefeff852d93992589c5fd48f3
Keenan <keenanjj13@gmail.com>
2025-05-05 13:51:41 -0600

    feat(blink): filtering out snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -17,6 +17,9 @@ end
 ---@type LazySpec
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "xzbdmw/colorful-menu.nvim",
+  },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -24,6 +27,22 @@ return {
     completion = {
       menu = {
         auto_show = true,
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx) return require("colorful-menu").blink_components_text(ctx) end,
+              highlight = function(ctx) return require("colorful-menu").blink_components_highlight(ctx) end,
+            },
+          },
+        },
+      },
+      trigger = {
+        prefetch_on_insert = true,
+        show_on_insert_on_trigger_character = true,
+        show_on_trigger_character = true,
+        show_on_keyword = true,
+        show_on_blocked_trigger_characters = {},
       },
     },
     fuzzy = {
@@ -39,7 +58,7 @@ return {
 
           if in_args then return is_lsp end
 
-          return true
+          return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
         end, items)
       end,
     },
